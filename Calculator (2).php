bc9635de957648df082b54ab21b1877056aaff93
tugcezkp <htgce@users.noreply.github.com>
2021-03-11 02:05:32 +0300

    OOP based.

--- Calculator (2).php
+++ Calculator (2).php
@@ -1,8 +1,9 @@
 <?php
+
 $lines = [
 	'40.00 Thijs Danny,Danny,Thijs,Stefan,Den',
-	'45.00 Danny Danny,Thijs,Stefan,Den',
-	 '36.00 Stefan Danny,Thijs,Stefan',
+	 '45.00 Danny Danny,Thijs,Stefan,Den',
+	'36.00 Stefan Danny,Thijs,Stefan',
 	 '40.00 Stefan Danny,Thijs,stefan,Den',
 	 '40.00 Danny Danny,Thijs,Stefan,Den',
 	 '12.00 Stefan Thijs,Stefan,Den',
@@ -20,8 +21,12 @@ $Calculator->printBill();
 class Calculator{
 
 	private $bills = [];
+	private $debtGraph = [];
+	private $resultMatrix = [];
+  private $optimizedResultMatrix = [];
 
-	public function __construct(array $bills){
+	public function __construct($bills){
+
 		foreach($bills as $bill_item){
 
 			$this->bills[] = new BillItem($bill_item);
@@ -43,132 +48,165 @@ class Calculator{
 		}
 	}
 
-    private function calculate()
+  private function calculate()
     {
-        // Implement me!
-		 $resultMatrix = ['thijs' => [], 'danny' => [], 'stefan' => [], 'den' => []]; // debtMatrix
-		 $lessTransactionResultMatrix = []; // optimizedMatrix
-
-		 $resultMatrix = $this->doSharingAmoungParticipant($resultMatrix); // calculateDebts
-     $lessTransactionResultMatrix = $this->eliminateUnnecessaryTransaction($resultMatrix); // optimizeTransactions
-     //print_r($resultMatrix);
-
-		 return $lessTransactionResultMatrix;
-	}
-
-private function eliminateUnnecessaryTransaction($filteredResultMatrix){ // optimizeTransactions
-		foreach ($filteredResultMatrix as $currentPersonName => $whomCurrentPersonOwe) {
-			if(is_array($whomCurrentPersonOwe) && sizeof($whomCurrentPersonOwe) > 0){
-				foreach ($whomCurrentPersonOwe as $thePersonNameWhoCurrentPersonOwe => $valueOfOweCurrentPersonToThePerson) {
-					if(is_array($filteredResultMatrix[$thePersonNameWhoCurrentPersonOwe]) && sizeof($filteredResultMatrix[$thePersonNameWhoCurrentPersonOwe]) > 0){
-							$peopleWhoCurrentPersonIOweOweTo = $filteredResultMatrix[$thePersonNameWhoCurrentPersonOwe];
-							$whomToIOwe = $this->findWhomToOweForASpecificPeople($filteredResultMatrix,$currentPersonName);
-							$findPeopleBothOfUsOweTo = array_intersect_key($whomToIOwe, $peopleWhoCurrentPersonIOweOweTo);
-						 if(sizeof($findPeopleBothOfUsOweTo) > 0){
-						 	$filteredResultMatrix = $this->eliminateTransactionBetweenThree($filteredResultMatrix,$currentPersonName,$thePersonNameWhoCurrentPersonOwe,$findPeopleBothOfUsOweTo);
-						}
-					}
+        $this->populateDebtGraph();
+				foreach ($this->debtGraph as $nodeItem) {
+						$this->resultMatrix[$nodeItem->getName()] = $nodeItem->getCreditorList();
 				}
-			}
-     }
-			$filteredResultMatrix = $this->eliminateMoreIfNeeded($filteredResultMatrix);
-			return $filteredResultMatrix;
 
-  }
+				//return $this->resultMatrix;
+				$this->optimizeTransactions();
+				//var_dump($this->optimizedResultMatrix);
+				return $this->optimizedResultMatrix;
+    }
 
-	private function eliminateMoreIfNeeded($filteredResultMatrix){ // reorderTransactions
-		$listOfPeopleWhoHasTransaction = array_filter($filteredResultMatrix, function($v, $k) {
-    return  sizeof($v) > 0;}, ARRAY_FILTER_USE_BOTH);
-    $listOfPeopleToCompare  = array_keys($listOfPeopleWhoHasTransaction);
-
-    $peopleNameWhoHasTheMaxNumTransaction = $this->findPeopleWhoTheMaxTransaction($filteredResultMatrix,$listOfPeopleToCompare);
-		$listOfPeopleToCompare = array_filter($listOfPeopleToCompare, function($v, $k) {return  $k != $peopleNameWhoHasTheMaxNumTransaction;}, ARRAY_FILTER_USE_BOTH);
-
-		$listOfPeopleWhoMakTransactionOweTo = $filteredResultMatrix[$peopleNameWhoHasTheMaxNumTransaction];
-
-		foreach ($listOfPeopleToCompare as $peopleNameWhoHasTransaction) {
-			$findPeopleBothOfUsOweTo = array_intersect_key($filteredResultMatrix[$peopleNameWhoHasTransaction], $listOfPeopleWhoMakTransactionOweTo);
-			foreach ($findPeopleBothOfUsOweTo as $peopleNameWeBothOweTo => $value) {
-				$howMuchMaxTransactionOwe = $this->howMuchIOwe($filteredResultMatrix, $peopleNameWhoHasTheMaxNumTransaction, $peopleNameWeBothOweTo);
-				$filteredResultMatrix[$peopleNameWhoHasTheMaxNumTransaction][$peopleNameWeBothOweTo] = 	$howMuchMaxTransactionOwe + 	$value ;
-				$filteredResultMatrix[$peopleNameWhoHasTransaction][$peopleNameWhoHasTheMaxNumTransaction] += $value;
-				unset(	$filteredResultMatrix[$peopleNameWhoHasTransaction][$peopleNameWeBothOweTo]);
-			}
-
-		}
-    return $filteredResultMatrix;
-	}
-
-
-	private function findPeopleWhoTheMaxTransaction($filteredResultMatrix, $listOfPeopleToCompare){
-    $countOfTransaction = 0;
-		foreach ($listOfPeopleToCompare as $peopleName) {
-			$transactionsForThisPeople = $filteredResultMatrix[$peopleName];
-			$sizeOfTransactions = sizeof($transactionsForThisPeople) ;
-			if($sizeOfTransactions > $countOfTransaction){
-				$peopleNameWhoHasTheMaxNumTransaction = $peopleName;
-				$countOfTransaction = $sizeOfTransactions;
-			}
-		}
-		return $peopleNameWhoHasTheMaxNumTransaction;
-	}
-
-	private function doSharingAmoungParticipant($resultMatrix){
+	private function populateDebtGraph(){
 			foreach($this->bills as $bill_item){
 				$sizeOfAttendees = sizeof($bill_item->attendees);
 				$amountToPay = $bill_item -> price / $sizeOfAttendees;
-				foreach ( $bill_item->attendees as $key => $value) {
-					$toWhom = $bill_item -> paid_by;
-					if($toWhom != $value){
-						$currentDebtOfToWhom = $resultMatrix[$toWhom][$value];
-						if($currentDebtOfToWhom > 0){
-							if($amountToPay < $currentDebtOfToWhom){
-								unset($resultMatrix[$value][$toWhom]);
-								$resultMatrix[$toWhom][$value] = $currentDebtOfToWhom - $amountToPay;
+				$creditor = $bill_item -> paid_by;
+				$currentNode = $this->getNode($creditor);
+				 foreach ( $bill_item->attendees as $debtor) {
+						if($currentNode->isEqual($debtor) != true){
+							$debtorNode = $this->getNode($debtor);
+							$creditorName = $currentNode->getName();
+							$debtAmountOfCreditor = $currentNode->getDebt($debtor);
+							if($debtAmountOfCreditor > 0){
+								if($debtAmountOfCreditor < $amountToPay){
+									$currentNode->removeDebt($debtorNode, $debtAmountOfCreditor);
+									$debtorNode->setCreditor($currentNode, $amountToPay-$debtAmountOfCreditor);
+								}else{
+									$currentNode->removeDebt($debtorNode,  $amountToPay);
+								}
 							}else{
-								unset($resultMatrix[$toWhom][$value]);
-								$resultMatrix[$value][$toWhom] = $amountToPay - $currentDebtOfToWhom;
+								$debtorNode->setCreditor($currentNode, $amountToPay);
 							}
-						}else{
-							 $resultMatrix[$value][$toWhom] += $amountToPay;
 						}
 					}
-				}
 			}
-			return $resultMatrix;
 		}
+	private function getNode($creditor){
+			$index = $this->getIndexOfNode($this->debtGraph, $creditor);
+			$currentNode;
+			if($index == -1){
+				$currentNode = new Node($creditor);
+				$currentNode->loan(0);
+				$currentNode->setDebtAmount(0);
+				array_push($this->debtGraph, $currentNode);
+			}else{
+				$currentNode = $this->debtGraph[$index];
+			}
+			return $currentNode;
+		}
+	private function getIndexOfNode(array $graph, $name) {
+	    for ($i = 0; $i < sizeof($graph); $i++) {
+				  $item = $graph[$i];
+	        if($item->isEqual($name)) return $i;
+	    }
+	    return -1;
+   }
+	private function optimizeTransactions(){
+		$maxBalanceNode = $this->finMaxBalanceNode($this->debtGraph);
+		$minBalanceNode = $this->findMinBalanceNode($this->debtGraph);
 
-		private function eliminateTransactionBetweenThree($filteredResultMatrix,$currentPersonName,$thePersonNameWhoCurrentPersonOwe,$findPeopleBothOfUsOweTo){
-			$peopleNameArray = array_keys($findPeopleBothOfUsOweTo);
-			foreach ($peopleNameArray as $name) {
-
-				$amountOfIOwe = $this->howMuchIOwe($filteredResultMatrix, $currentPersonName,$thePersonNameWhoCurrentPersonOwe);
-					if($amountOfIOwe > 0){
-					$amountOfOweOfIOwe = $this->howMuchIOwe($filteredResultMatrix,$thePersonNameWhoCurrentPersonOwe,$name);
-					$amountOfMe = $this->howMuchIOwe($filteredResultMatrix,$currentPersonName,$name);
-						if($amountOfIOwe > $amountOfOweOfIOwe){
-						  unset($filteredResultMatrix[$thePersonNameWhoCurrentPersonOwe][$name]);
-						  $filteredResultMatrix[$currentPersonName][$thePersonNameWhoCurrentPersonOwe] = $amountOfIOwe - $amountOfOweOfIOwe;
-						  $filteredResultMatrix[$currentPersonName][$name] = $amountOfMe +  $amountOfOweOfIOwe;
-						}else{
-						  $filteredResultMatrix[$thePersonNameWhoCurrentPersonOwe][$name] =  $amountOfOweOfIOwe - $amountOfIOwe;
-						  unset($filteredResultMatrix[$currentPersonName][$thePersonNameWhoCurrentPersonOwe]);
-						  $filteredResultMatrix[$currentPersonName][$name] = $amountOfMe + $amountOfIOwe;
-					 }
-			    }
-	 	 }
-		return $filteredResultMatrix;
+		if($maxBalanceNode == null && $minBalanceNode == null)
+    return ;
+		else if($maxBalanceNode->isEqual($minBalanceNode))
+		return ;
+		else{
+			$min = $this->getMin(-($minBalanceNode->getBalance()), $maxBalanceNode->getBalance());
+      $minBalanceNode->removeDebt($maxBalanceNode, $min);
+			$this->optimizedResultMatrix = $this->populateOptimizedDebtMatrix($minBalanceNode, $maxBalanceNode, $min, $this->optimizedResultMatrix);
+			$this->optimizeTransactions();
+		}
+	}
+	private function finMaxBalanceNode($graph){
+		$max = 0;
+		$max ;
+		foreach ($graph as $node) {
+			if($node->getBalance() > $max){
+				$max = $node->getBalance();
+				$maxNode =  $node;
+			}
+		}
+		return $maxNode;
+	}
+	private function findMinBalanceNode($graph){
+		$min = 0;
+		$minNode;
+		foreach ($graph as $node) {
+			if($node->getBalance() < $min){
+				$min = $node->getBalance();
+				$minNode =  $node;
+			}
+		}
+		return $minNode;
+	}
+	private function populateOptimizedDebtMatrix($debtor,$creditor,$amount, $matrix){
+	    $creditorList = $matrix[$debtor->getName()];
+			$creditorList[$creditor->getName()] += $amount ;
+			$matrix[$debtor->getName()] = $creditorList;
+			return $matrix;
+		}
+	private function getMin($value1, $value2){
+		return ($value1 < $value2) ? $value1 : $value2;
 	}
 
-		private function findWhomToOweForASpecificPeople($matrixToBeSearched, $peopleName){ // findPeopleIOwe
-			return $matrixToBeSearched[$peopleName];
-		}
-
-		private function howMuchIOwe($matrix, $nameOfDebtor, $nameOfIOwe){
-		  return	$matrix[$nameOfDebtor][$nameOfIOwe];
-		}
 
 }
+
+class Node{
+	public $name;
+	public $creditAmount;
+	public $debtAmount;
+	public $creditorList = [];
+
+	public function __construct($name){
+		$this->name = $name;
+	}
+	public function getName(){
+		return $this->name;
+	}
+	public function getCreditorList(){
+		return $this->creditorList;
+	}
+	public function setDebtAmount($debtAmount){
+		 $this->debtAmount = $debtAmount;
+	}
+	public function setCreditor($creditor, $amountToPay){
+		$creditor->loan($amountToPay);
+		$this->creditorList[$creditor->getName()] += $amountToPay;
+	  $this->debtAmount += $amountToPay;
+	}
+	/*Get debt amount to the debtor*/
+	public function getDebt($debtor){
+	 return	$this->creditorList[$debtor];
+	}
+	public function getBalance(){
+		$balanceVal = $this->creditAmount - $this->debtAmount;
+		if(round($balanceVal) == 0) return 0;
+		else return $balanceVal;
+	}
+	/* To remove already given credit of mine*/
+	public function removeDebt($debtorNode, $debtToRemove){
+		$this->debtAmount -= $debtToRemove;
+		$debtorNode->loan(-$debtToRemove);
+		$this->creditorList[$debtorNode->name] -= $debtToRemove;
+		if($this->creditorList[$debtorNode->name] == 0) unset($this->creditorList[$debtorNode->name]);
+	}
+	/*Give credit to someone*/
+	public function loan($creditAmount){
+		$this->creditAmount += $creditAmount;
+	}
+	public function toString(){
+		echo "Name: $this->name, Debt Amount: $this->debtAmount , Credit Amount: $this->creditAmount".PHP_EOL;
+	}
+	public function isEqual($nameToCompare){
+		return $this->name == $nameToCompare;
+	}
+ }
+
 
 
 class BillItem{
@@ -183,9 +221,10 @@ class BillItem{
 		$this->price = (float) $data[0];
 		$this->paid_by = strtolower($data[1]);
 		foreach(explode(',', $data[2]) as $debtor){
+
 			$this->attendees[] = strtolower($debtor);
 		}
-
 	}
 }
+
 ?>
